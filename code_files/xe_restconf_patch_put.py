29a8db820ecdce01c2a1385d6512876196a9db0c
Viacheslav Isaev <visaev@nil.com>
2020-12-08 15:37:19 +0100

    Optimizing output.

--- code_files/xe_restconf_patch_put.py
+++ code_files/xe_restconf_patch_put.py
@@ -21,59 +21,46 @@ loopback_interface_data_primary_address = """<Loopback><name>1</name><ip><addres
 </address><mask>255.255.255.0</mask></primary></address></ip></Loopback>"""
 
 loopback_interface_data_secondary_address = """
-<Loopback>
-    <name>1</name>
-    <ip>
-        <address>
-            <secondary>
-                    <address>10.101.1.3</address>
-                    <mask>255.255.255.0</mask>
-                    <secondary/>
-            </secondary>
-        </address>
-    </ip>
+<Loopback><name>1</name>
+    <ip><address><secondary>
+        <address>10.101.1.3</address>
+        <mask>255.255.255.0</mask>
+        <secondary/>
+    </secondary></address></ip>
 </Loopback>"""
 
 response = requests.request("DELETE", url+'/Loopback=1', verify=False, auth=auth)
 
-print("\nCreating interface")
 response = requests.request("PUT", 
                         url+"/Loopback=1", 
                         data=loopback_interface_data_min,
                         headers=xml_headers, 
                         verify=False, 
                         auth=auth)
-print("PUT status code: "+str(response.status_code))
-print("PUT status response:  \n"+response.text)
+print("Creating interface. PUT status code: "+str(response.status_code))
 
-print("\nConfiguring interface with PATCH.")
 response = requests.request("PATCH", 
                         url+"/Loopback", 
                         data=loopback_interface_data_primary_address,
                         headers=xml_headers, 
                         verify=False, 
                         auth=auth)
-print("PATCH status code: "+str(response.status_code))
-print("PATCH status response:  \n"+response.text)
+print("Configuring primary address. PATCH status code: "+str(response.status_code))
 
-print("\nAttempt to use PUT to configure interface.")
 response = requests.request("PUT", url+"/Loopback=1", 
                         data=loopback_interface_data_secondary_address, 
                         headers=xml_headers, 
                         verify=False, 
                         auth=auth)
-print("PUT status code: "+str(response.status_code))
-print("PUT status response:  \n"+response.text)
+print("Configuring secondary address. PUT status code: "+str(response.status_code))
 
-print("\nUsing PATCH to add secondary interface.")
 response = requests.request("PATCH",
                         url+"/Loopback=1", 
                         data=loopback_interface_data_secondary_address, 
                         headers=xml_headers, 
                         verify=False, 
                         auth=auth)
-print("PATCH status code: "+str(response.status_code))
-print("PATCH status response:  \n"+response.text)
+print("Configuring secondary address. PATCH status code: "+str(response.status_code))
 
 response = requests.request("GET",
                         url+"/Loopback=1", 
